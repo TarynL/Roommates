43aa20ae13441370d2bb558a4cf11c76f28aa721
TarynL <tarlytle@gmail.com>
2021-06-11 15:03:29 -0500

    working on unassigned chores

---
+++ Repositories/RoommateRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roommates.Repositories
+{
+    class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT rm.FirstName, rm.RentPortion, r.Name  FROM Roommate rm Join Room r on rm.RoomId = r.id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Roommate roommate = null;
+
+                    // If we only expect a single row back from the database, we don't need a while loop.
+                    if (reader.Read())
+                    {
+                        roommate = new Roommate
+                        {
+                            Id = id,
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion"))
+                            
+                        };
+                    }
+
+                    reader.Close();
+
+                    return roommate;
+                }
+            }
+        }
+    }
+}
