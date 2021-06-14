74a7578fc18e043d525f8a4ebf839d574a408632
TarynL <tarlytle@gmail.com>
2021-06-14 13:07:49 -0500

    ability to update rooms

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Roommates.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Roommates
 {
@@ -137,9 +138,9 @@ namespace Roommates
                         Console.WriteLine("Please select Chore Id you want to assign: ");
                         int choreRoommate = int.Parse(Console.ReadLine());
 
-                        
+
                         List<Roommate> roommateList = roommateRepo.GetAll();
-                        foreach(Roommate rm in roommateList)
+                        foreach (Roommate rm in roommateList)
                         {
                             Console.WriteLine($"{rm.FirstName} has an Id of {rm.Id}");
                         }
@@ -152,6 +153,31 @@ namespace Roommates
                         Console.Write("Press any key to continue");
                         Console.ReadKey();
                         break;
+
+                    case ("Update a room"):
+                        List<Room> roomOptions = roomRepo.GetAll();
+                        foreach (Room r in roomOptions)
+                        {
+                            Console.WriteLine($"{r.Id} - {r.Name} Max Occupancy({r.MaxOccupancy})");
+                        }
+
+                        Console.Write("Which room would you like to update? ");
+                        int selectedRoomId = int.Parse(Console.ReadLine());
+                        Room selectedRoom = roomOptions.FirstOrDefault(r => r.Id == selectedRoomId);
+
+                        Console.Write("New Name: ");
+                        selectedRoom.Name = Console.ReadLine();
+
+                        Console.Write("New Max Occupancy: ");
+                        selectedRoom.MaxOccupancy = int.Parse(Console.ReadLine());
+
+                        roomRepo.Update(selectedRoom);
+
+                        Console.WriteLine("Room has been successfully updated");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
 
 
                     case ("Exit"):
@@ -178,6 +204,7 @@ namespace Roommates
                 "Search for roommate",
                 "Show unassigned chores",
                 "Assign a chore",
+                "Update a room",
                 "Exit"
             };
 
